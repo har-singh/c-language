9f862c262849f049916a8c09f82ecaf63fc36477
Harpreet Singh <harpreet.singh@DC1MSPDVLNX001V.MSP.LOCAL>
2020-03-05 11:13:45 +0000

    update

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#05Mar20: With Guru ji's kirpa Harpreet started with C today.
+#https://devcentral.f5.com/s/articles/Hands-on-C-argc-and-argv-from-main-function-explained 
+
 int main(int argc, char * argv[]) {
   printf("Hello World\n");
 }
